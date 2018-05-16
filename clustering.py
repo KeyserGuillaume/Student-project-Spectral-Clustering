f9bc314a7ab9095f669a69a992c2fcd0c3e1d938
KeyserGuillaume <guillaume-dekeyser@hotmail.fr>
2018-05-16 20:03:26 +0200

    added generation functions

--- clustering.py
+++ clustering.py
@@ -12,11 +12,10 @@ def unnormalized_spectral_clustering(W,k):
     L = unnormalized_laplacian(W)
     eigenvalues, eigenvectors = np.linalg.eig(L)
     index_sorted = np.argsort(eigenvalues)
- #    plt.plot(eigenvalues[index_sorted]);plt.show()
+    # plt.plot(eigenvalues[index_sorted]);plt.show()
     k_first_eigenvectors = eigenvectors[:,index_sorted[:k]]
-    kmeans = KMeans(n_clusters=k).fit(np.transpose(k_first_eigenvectors.T))
+    kmeans = KMeans(n_clusters=k).fit(k_first_eigenvectors)
     prediction = kmeans.labels_
-    print("pred", prediction)
     clusters = {}
     for i in range(k):
         clusters[i] = np.where(prediction == i)
@@ -31,7 +30,7 @@ def normalized_laplacian_sym(W):
     d = np.sum(W,axis=1)
     D_inverse_root = np.diag(1/np.square(d))
     return np.identity(W.shape[0]) - D_inverse_root*W*D_inverse_root
-    
+
 def normalized_spectral_clustering(W,k):
     Lrw = normalized_laplacian_rw(W)
     eigenvalues, eigenvectors = np.linalg.eig(Lrw)
@@ -57,22 +56,24 @@ def normalized_spectral_clustering_bis(W,k):
     return eigenvalues[index_sorted[:k]], k_first_eigenvectors,clusters
 
 if __name__ == "__main__":
-    data,y = generation.gen_1d_gaussian_mixture(nbex=200)
+    # data,y = generation.gen_1d_gaussian_mixture(nbex=200)
+    # data, y = generation.gen_arti(nbex=200, data_type=1)
+    data,y = generation.random_walks(4, 200, step=0.1, d=np.array([0.1, 0.2]))
     S = graph.gaussian_similarity_matrix(data)
     W_fullyconnected = graph.fully_connected_graph(S)
     W_neighbors = graph.k_nearest_neighbors_graph(S,10)
     
     #Unnormalized laplacian 
     eigenvalues, eigenvectors, clusters = unnormalized_spectral_clustering(W_neighbors,4)
-    #eigenvalues, eigenvectors, clusters = normalized_spectral_clustering(W_neighbors,4)
-    #eigenvalues, eigenvectors, clusters = normalized_spectral_clustering_bis(W_neighbors,4)
+    # eigenvalues, eigenvectors, clusters = normalized_spectral_clustering(W_neighbors,4)
+    # eigenvalues, eigenvectors, clusters = normalized_spectral_clustering_bis(W_neighbors,4)
     
     import matplotlib.pyplot as plt
     plt.figure(1)
-    plt.scatter(range(len(data)), data)
+    plt.scatter(data[:,0], data[:,1], edgecolors='face')
     plt.figure(2)
-    colors = "bgrcmykw"
+    colors = "byrmcgkw"
     for i in clusters:
         cluster = clusters[i][0]
-        plt.scatter(range(len(cluster)), data[cluster], c=colors[i])
+        plt.scatter(data[cluster,0], data[cluster,1], c=colors[i], edgecolors='face')
     plt.show()
--- generation.py
+++ generation.py
@@ -11,3 +11,64 @@ def gen_1d_gaussian_mixture(centers = [-3,-1,1,3],sigma=0.1,nbex=1000):
     data=data[idx]
     y=y[idx]
     return data.reshape(-1,1),y.reshape(-1,1)
+
+ # recyclee du tp 3 de malap
+def gen_arti(centerx=1,centery=1,sigma=0.1,nbex=1000,data_type=0,epsilon=0.02):
+    """ Generateur de donnees,
+        :param centerx: centre des gaussiennes
+        :param centery:
+        :param sigma: des gaussiennes
+        :param nbex: nombre d'exemples
+        :param data_type: 0: melange 2 gaussiennes, 1: melange 4 gaussiennes, 2:echequier
+        :param epsilon: bruit dans les donnees
+        :return: data matrice 2d des donnnes,y etiquette des donnnees
+    """
+    if data_type==0:
+         #melange de 2 gaussiennes
+         xpos=np.random.multivariate_normal([centerx,centerx],np.diag([sigma,sigma]),int(nbex//2))
+         xneg=np.random.multivariate_normal([-centerx,-centerx],np.diag([sigma,sigma]),int(nbex//2))
+         data=np.vstack((xpos,xneg))
+         y=np.hstack((np.ones(nbex//2),-np.ones(nbex//2)))
+    if data_type==1:
+        #melange de 4 gaussiennes
+        xpos=np.vstack((np.random.multivariate_normal([centerx,centerx],np.diag([sigma,sigma]),int(nbex//4)),np.random.multivariate_normal([-centerx,-centerx],np.diag([sigma,sigma]),int(nbex/4))))
+        xneg=np.vstack((np.random.multivariate_normal([-centerx,centerx],np.diag([sigma,sigma]),int(nbex//4)),np.random.multivariate_normal([centerx,-centerx],np.diag([sigma,sigma]),int(nbex//4))))
+        data=np.vstack((xpos,xneg))
+        y=np.hstack((np.ones(nbex//2),-np.ones(int(nbex//2))))
+
+    if data_type==2:
+        #echiquier
+        data=np.reshape(np.random.uniform(-4,4,2*nbex),(nbex,2))
+        y=np.ceil(data[:,0])+np.ceil(data[:,1])
+        y=2*(y % 2)-1
+    # un peu de bruit
+    data[:,0]+=np.random.normal(0,epsilon,nbex)
+    data[:,1]+=np.random.normal(0,epsilon,nbex)
+    # on melange les donnees
+    idx = np.random.permutation((range(y.size)))
+    data=data[idx,:]
+    y=y[idx]
+    return data,y
+    
+def random_walks(k, nbex, step=0.005, noise=0.005, d=None):
+    """
+    L'idee est d'avoir k marches aleatoires
+    """
+    data = np.array([]).reshape(-1,2)
+    y = np.array([])
+    timesToWalk = nbex/k*np.ones(nbex)
+    for i in range(k):
+        x = np.random.rand(2,1).reshape(-1,2)
+        if d.any():
+            d = d.reshape(-1,2)
+        else:
+            d = (np.random.rand(2,1)-0.5).reshape(-1,2)
+        data = np.vstack((data,x))
+        for t in range(int(timesToWalk[i]-1)):
+            x+=step*d+noise*(np.random.rand(2,1)-0.5).reshape(-1,2)
+            data = np.vstack((data,x))
+        y = np.concatenate((y,i*np.ones(timesToWalk[i])))
+    idx = np.random.permutation(nbex)
+    data=data[idx]
+    y=y[idx]
+    return data, y.reshape(-1,1)
